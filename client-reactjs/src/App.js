0be5f03b22ebfc68552f33cfe665950571142062
TranNhan <trannhan.s3@gmail.com>
2022-05-17 20:08:13 +0700

    hoan thanh UI

--- client-reactjs/src/App.js
+++ client-reactjs/src/App.js
@@ -1,25 +1,27 @@
-import { Divider, Typography } from "antd";
-import "./App.css";
-import TodoList from "./components/TodoList";
+import { Divider, Typography } from 'antd';
+import './App.css';
+import Filters from './components/Filters';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div
       style={{
         width: 500,
-        margin: "0 auto",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "white",
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
         padding: 20,
-        boxShadow: "0 0 10px 4px #bfbfbf",
+        boxShadow: '0 0 10px 4px #bfbfbf',
         borderRadius: 5,
-        height: "90vh",
+        height: '90vh',
       }}
     >
-      <Typography.Title style={{ textAlign: "center" }}>
+      <Typography.Title style={{ textAlign: 'center' }}>
         TODO APP with REDUX
       </Typography.Title>
+      <Filters />
       <Divider />
       <TodoList />
     </div>
